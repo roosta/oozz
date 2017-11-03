0ce21f3b9942eee93e8cd92ec344fa95d128193f
Daniel Berg <mail@roosta.sh>
2017-11-03 20:26:06 +0100

    Fix integration tests after char updates

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -10,20 +10,27 @@ fn no_args() {
 #[test]
 fn yellow_bold() {
     let expected: Vec<&str> = include_str!("yellow_bold.txt").lines().collect();
-    let result = oozz::run("test", "yellow", true, false, 0).unwrap();
+    let result = oozz::run("_livid", "yellow", true, false, 0).unwrap();
     assert_eq!(result, expected);
 }
 
 #[test]
 fn blue_centered() {
     let expected: Vec<&str> = include_str!("blue_centered.txt").lines().collect();
-    let result = oozz::run("test", "blue", false, true, 159).unwrap();
+    let result = oozz::run("'hello'", "blue", false, true, 159).unwrap();
     assert_eq!(result, expected);
 }
 
 #[test]
 fn all_args() {
     let expected: Vec<&str> = include_str!("all_args.txt").lines().collect();
-    let result = oozz::run("test", "magenta", true, true, 159).unwrap();
+    let result = oozz::run("./!1234567890", "magenta", true, true, 319).unwrap();
+    assert_eq!(result, expected);
+}
+
+#[test]
+fn dollar_quote() {
+    let expected: Vec<&str> = include_str!("dollar_quote.txt").lines().collect();
+    let result = oozz::run("\"$\"", "green", false, false, 159).unwrap();
     assert_eq!(result, expected);
 }
