aad1a129b723ded7956704105ca344fbbc906ad0
Daniel Berg <mail@roosta.sh>
2017-11-03 19:18:41 +0100

    Add new chars to LETTERS, update valid re

--- src/lib.rs
+++ src/lib.rs
@@ -15,7 +15,7 @@ const LETTER_HEIGHT: usize = 17;
 const OOZZ_HEIGHT: usize = 22;
 
 // Available letters in font
-pub const LETTERS: &'static str = "abcdefghijklmnopqrstuvwxyz.! /\'\"_$";
+pub const LETTERS: &'static str = "abcdefghijklmnopqrstuvwxyz.! /\'\"_$1234567890";
 
 const CHARS: &'static str = include_str!("../resources/chars.latin1");
 const EXTRA: &'static str = include_str!("../resources/extra.latin1");
@@ -31,7 +31,7 @@ const CHAR_WIDTH: usize = 18;
 /// validate input characters, ensure they are part of the allowed set
 pub fn valid_chars(v: String) -> Result<(), String> {
     lazy_static! {
-        static ref VALID_RE: Regex = Regex::new(r"[^a-zA-Z\s!\./'\042_\$]").unwrap();
+        static ref VALID_RE: Regex = Regex::new(r"[^a-zA-Z\s!\./'\042_\$0-9]").unwrap();
     }
     match VALID_RE.captures(&v[..]) {
         None => Ok(()),
